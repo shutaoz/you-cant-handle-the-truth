a7548a568195b1c49da8ab6c0a8970af08f990ed
shutaozhang <shutao76@uw.edu>
2020-11-21 21:13:24 -0800

    Case1

--- src/main/java/HandleTruth.java
+++ src/main/java/HandleTruth.java
@@ -1,8 +1,12 @@
 import java.util.*;
 
 public class HandleTruth {
-    public static void wordCount() {
-        String msg = "son we live in a world that has walls and those walls have to be guarded by men with guns whos gonna do it you you lieutenant weinberg i have a greater responsibility than you can possibly fathom you weep for santiago and you curse the marines you have that luxury you have the luxury of not knowing what i know that santiagos death while tragic probably saved lives and my existence while grotesque and incomprehensible to you saves lives you dont want the truth because deep down in places you dont talk about at parties you want me on that wall you need me on that wall we use words like honor code loyalty we use these words as the backbone of a life spent defending something you use them as a punchline i have neither the time nor the inclination to explain myself to a man who rises and sleeps under the blanket of the very freedom that i provide and then questions the manner in which i provide it i would rather you just said thank you and went on your way otherwise i suggest you pick up a weapon and stand a post either way i dont give a damn what you think you are entitled to";
+
+    public static String[] wordCount(String msg) {
+        //String msg = "son we live in a world that has walls and those walls have to be guarded by men with guns whos gonna do it you you lieutenant weinberg i have a greater responsibility than you can possibly fathom you weep for santiago and you curse the marines you have that luxury you have the luxury of not knowing what i know that santiagos death while tragic probably saved lives and my existence while grotesque and incomprehensible to you saves lives you dont want the truth because deep down in places you dont talk about at parties you want me on that wall you need me on that wall we use words like honor code loyalty we use these words as the backbone of a life spent defending something you use them as a punchline i have neither the time nor the inclination to explain myself to a man who rises and sleeps under the blanket of the very freedom that i provide and then questions the manner in which i provide it i would rather you just said thank you and went on your way otherwise i suggest you pick up a weapon and stand a post either way i dont give a damn what you think you are entitled to";
+
+        //check for the validity of the parameter
+        if(msg == null) return null;
 
         // split the message into word set
         String[] words = msg.split(" ");
@@ -29,9 +33,13 @@ public class HandleTruth {
             values.add(entry.getKey());
             sortedMap.put(entry.getValue(), values);
         }
-
+        String[] retVal = new String[sortedMap.size()];
+        int count = 0;
         for (Map.Entry<Integer, Set<String>> entry : sortedMap.entrySet()) {
             System.out.println(entry);
+            retVal[count++] = entry.toString();
+
         }
+        return retVal;
     }
 }
--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -1,8 +1,25 @@
+import org.junit.Assert;
 import org.junit.Test;
+
 
 public class HandleTruthTest {
     @Test
     public void makeshiftTest() {
-        HandleTruth.wordCount();
+        String msg = "son we live in a world that has walls and those walls have to be guarded by men with guns whos gonna do it you you lieutenant weinberg i have a greater responsibility than you can possibly fathom you weep for santiago and you curse the marines you have that luxury you have the luxury of not knowing what i know that santiagos death while tragic probably saved lives and my existence while grotesque and incomprehensible to you saves lives you dont want the truth because deep down in places you dont talk about at parties you want me on that wall you need me on that wall we use words like honor code loyalty we use these words as the backbone of a life spent defending something you use them as a punchline i have neither the time nor the inclination to explain myself to a man who rises and sleeps under the blanket of the very freedom that i provide and then questions the manner in which i provide it i would rather you just said thank you and went on your way otherwise i suggest you pick up a weapon and stand a post either way i dont give a damn what you think you are entitled to";
+        HandleTruth.wordCount(msg);
+
+        //case 1 when the parameter is empty
+        //when the parameter is null, wordCount should return null as well
+        Assert.assertNull(HandleTruth.wordCount(null));
+
+
+        //case 2 when the parameter is valid, it should return the expected stirng
+
+
+        //case 3
+
+        //case 4
+
+        //case 5
     }
 }
